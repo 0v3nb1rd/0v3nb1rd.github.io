cf0d3f23dae8bac33f8565c09ddf231c60be3b75
0v3nb1rd <0v3nb1rd@gmail.com>
2022-04-28 11:44:42 +0300

    fix styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,9 +15,9 @@ export default function Footer() {
   // const { copiright } = data.site.siteMetadata
 
   return (
-    <footer className="items-center pb-12 bg-neutral text-neutral-content relative">
+    <footer className="items-center text-neutral-content relative">
       <svg
-        className="absolute top-0 w-full -mt-6 sm:-mt-16 h-6 sm:h-16 "
+        className="absolute top-0 w-full  h-6 sm:h-16 "
         preserveAspectRatio="none"
         viewBox="0 0 1440 54"
       >
@@ -26,7 +26,7 @@ export default function Footer() {
           d="M0 22L120 16.7C240 11 480 1.00001 720 0.700012C960 1.00001 1200 11 1320 16.7L1440 22V54H1320C1200 54 960 54 720 54C480 54 240 54 120 54H0V22Z"
         />
       </svg>
-      <div className="flex flex-col">
+      <div className="flex flex-col pb-16 mt-16 bg-neutral">
         <div className="md:hidden mt-7 mx-auto w-11 h-px rounded-full"></div>
 
         <div className="mt-4 md:mt-0 flex flex-col md:flex-row">
