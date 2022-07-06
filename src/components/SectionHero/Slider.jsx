2a4f8b486c4b7a25e90c630325706bb08ecd6084
0v3nb1rd <0v3nb1rd@gmail.com>
2022-07-06 15:06:28 +0300

    rm. Hero slider on unmount

--- src/components/SectionHero/Slider.jsx
+++ src/components/SectionHero/Slider.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 import { motion } from 'framer-motion'
 
 export const Slider = ({ className, children, ...props }) => {
-  const faceCount = 4 // Counts of face images
+  const faceCount = 14 // Counts of face images
   const [face, setFace] = React.useState(1)
 
   React.useEffect(() => {
-    setInterval(() => {
+    const intervalId = setInterval(() => {
       setFace((face) => {
         face === faceCount ? setFace(1) : setFace(face + 1)
       })
     }, 1000)
+
+    return () => clearInterval(intervalId)
   }, [])
 
   return (
     <div className={className}>
       <motion.img
         className="h-full mx-auto"
-        src={`/BigHead/${1}.svg`}
+        src={`/BigHead/${face}.svg`}
         alt="Big head image"
         key={face}
         initial={{ opacity: 0, y: 20 }}
